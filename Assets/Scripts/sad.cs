ff69d756f312137d83c6566ae8ba731644fcbc02
topinkayttaja <topi.suhonen@student.eduko.fi>
2026-03-02 12:46:31 +0200

    Merge branch 'UI'

--- Assets/Scripts/sad.cs
+++ Assets/Scripts/sad.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class test2 : MonoBehaviour
+public class sad : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Testing. Testing.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Testing. Testing.");
     }
 }
---
+++ Assets/Scripts/test2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Testing. Testing.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Testing. Testing.");
+    }
+}
